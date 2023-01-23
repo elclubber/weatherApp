0cee773543a7388a603c1b9d0c43227956933318
kenny <k.oozageer@providus.mu>
2023-01-23 13:39:52 +0400

    remove unused folder

--- toSend/src/utils/constants.ts
+++
@@ -1,5 +0,0 @@
-export const API_URLS = {
-    GET_WEATHER: 'http://api.openweathermap.org/data/2.5/weather?q='
-};
-
-export const API_KEY = 'f825344b0cf0672c689378549f9868db';
--- toSend/src/utils/request.ts
+++
@@ -1,8 +0,0 @@
-export function requestGet(url: RequestInfo | URL) {
-    const options = {
-        method: 'GET',
-    };
-    return fetch(url, options)
-        .then(response => response.json())
-        .then((data) => data);
-}
